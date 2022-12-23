e933a0c50e76951cb4e7e7f7ba5983983be4af6a
Gector113 <90830467+Gector113@users.noreply.github.com>
2022-12-23 22:15:27 +0200

    Delete Lab3 directory

--- Lab3/Configuration.java
+++
@@ -1,7 +0,0 @@
-package org.fpm.di.config;
-
-import org.fpm.di.binder.Binder;
-
-public interface Configuration {
-    void configure(Binder binder);
-}
--- Lab3/Environment.java
+++
@@ -1,8 +0,0 @@
-package org.fpm.di.env;
-
-import org.fpm.di.config.Configuration;
-import org.fpm.di.container.Container;
-
-public interface Environment {
-    Container configure(Configuration configuration);
-}
