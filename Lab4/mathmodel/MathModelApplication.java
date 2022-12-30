ba142334424ea833e0e332df832c71383014df37
Gector113 <90830467+Gector113@users.noreply.github.com>
2022-12-30 18:24:44 +0200

    Add files via upload

---
+++ Lab4/mathmodel/MathModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.mathmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathModelApplication.class, args);
+    }
+
+}
